fb57f00ff061e018c68259c315f511ad901cc455
Anton Onizhuk <aonyshuk@atimi.com>
2018-12-21 18:18:02 +0200

    Fixed syntax

--- step.sh
+++ step.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ex
 
 #
@@ -10,14 +11,6 @@ if [ "${strict}" = "yes" ] ; then
   FLAGS=$FLAGS' --strict'
 fi
 
-if [ -s "${lint_config_file}" ] ; then
-  FLAGS=$FLAGS' --config '"${lint_config_file}"  
-fi
-
-if [ "${reporter}" != "none" ] ; then
-  FLAGS=$FLAGS' --reporter '"${reporter}"  
-fi
-
 #
 # Folder
  
@@ -25,18 +18,17 @@ if [ -z "${linting_path}" ] ; then
   linting_path="."
 fi
 
-
 #
 # Call
 
 pushd "${linting_path}"
 
-if [ -f "${executable_path}" ] ; then
-    $"executable_path" ${FLAGS}
-elif [ "${executable_path}" == "swiftlint" ]
-    swiftlint lint ${FLAGS}
+if [ -s "${executable_path}" ] ; then
+  "${executable_path}" --reporter "${reporter}" --config "${lint_config_file}" ${FLAGS}
+elif [ "${executable_path}" == "swiftlint" ] ; then
+  swiftlint lint --reporter "${reporter}" "${FLAGS}"
 else
-  echo " [!] SwiftLint executable should be a path to file or a system swiftlint call. Received: '${executable_path}'"
+  echo " [!] SwiftLint executable should be a path to file or a system swiftlint call. Received: '$executable_path'"
   exit 1
 fi
 
